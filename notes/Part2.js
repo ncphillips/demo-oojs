a2fcf49a5af2dea26da4470006b563252c13d1a4
ncphillips <ncphillips.19@gmail.com>
2016-06-08 21:14:52 -0300

    Note updates

--- notes/Part2.js
+++ notes/Part2.js
@@ -6,8 +6,8 @@
  * patterns that are not immediately apparent in Javascript.
  *
  * 1. A Quick Review
- *	a) Lexical Scope
- *	b) Contextual Scope
+ *	a) Written Scope
+ *	b) Runtime Scope
  *  c) Pass-by-Reference
  *  d) Closure
  *  e) Prototype
@@ -22,35 +22,34 @@
  */
 
 /**
- * Lexical Scope
- * Access based on write time structure
+ * Written Scope (Lexical Scope
+ * Access variables based on the written structure of the code.
  */
 
-// `sound` is accessed from the lexical scope. The scope defined at write-time.
 var sound = "LOUD NOISES";
 
-function speak() {
-    return sound;
+function lexSpeak() {
+    return sound; // lexical scope.
 }
 
-speak(); // LOUD NOISES
+lexSpeak(); // LOUD NOISES
 
 // Lexical scope does not depend on the context in which a function is called.
-// This means if we have another function call `speak` with it's own definition
-// of `sound`, `speak` will still access the global `sound`.
-function speak2() {
+// Lexical scope depends on the context in which a function is written.
+function lexSpeakWrapper() {
     var sound = "test";
     return speak();
 }
 
-speak2(); // LOUD NOISES
+lexSpeakWrapper(); // LOUD NOISES
+
 
 /**
- * Contextual scope with `this`.
+ * Runtime scope with `this`.
  * Access based on the context in which a function is called.
  */
 sound = "GLOBAL";
-funciton speak() {
+function speak() {
 	return this.sound;
 }
 
@@ -68,85 +67,6 @@ eg.speak(); // EG
 // We can tell functions in which context we want them to be called, using `call`.
 speak.call(eg); // EG
 eg.speak.call(this); // GLOBAL
-
-/**
- * Pass by Reference
- */
-var sound = "LOUD NOISES";
-
-function getSpeakForSound(sound) {
-	return function speak() {
-		// Lexical scope is still used to access sound, however the `sound` we're looking for
-		// is no declared within the functions scope, not in the global scope.
-        console.log(sound);
-    }
-}
-
-speak1 = getSpeakForSound();
-speak1(); // undefined
-// Because we didn't pass any value into `getSpeakForSoudn`, the echoed value is `undefined`.
-
-speak2 = getSpeakForSound(sound);
-speak2(); // LOUD NOISES
-// Now it can access the string value.
-
-speak1(); // undefined
-speak2(); // LOUD NOISES
-// These are two completely separate functions, taking up their own space in memory.
-
-sound = "LOUD NOISES";
-speak2 = getSpeakForSound(sound);
-speak2(); // LOUD NOISES;
-sound = "OTHER NOISES";
-speak2(); // LOUD NOISES
-
-// The output did not change, because Javascript is pass-by-value, so there's actually
-// two variables for `sound` now.
-
-/**
- * Closure
- */
-// Look at this stub of `getSpeakForSound`.
-function getSpeakForSound(sound) { }
-
-// This copies `sound`, passes it to `getSpeakForSound`.
-// In this situation, `sound` is immediately destroyed by the garbage collector
-// once `getSpeakForSound` stops executing.
-getSpeakForSound(sound);
-
-// If the garbage collection destroys `sound` after `getSpeakForSound` finishes
-// executing, then why does the `speak` function in this case still work?
-function getSpeakForSound(sound) {
-	return function speak() { return sound; }
-}
-
-sound = "Test";
-speak = getSpeakForSound(sound);
-speak("Test");
-
-// This is closure. Closure is what allows functions to access their lexical scope
-// long after it should have been gone. We say that the `speak` function created
-// at runtime, has a closure over the state of `getSpeakForSound` at run
-
-
-
-
-function getSpeakForSound(sound) {
-	// Javascript passes variables `by value`. This means the `sound` in the function is
-	// a copy of the `sound` in the global scope.
-	// So why can this `speak` function reference the `sound` passed to the function?
-	return function speak() {
-		// Normally, `sound` would be deallocated after `getSpeakForSound` finishes executing.
-		// However, a new `speak` function is created at run-time, and returned to the caller.
-		// And since this `speak` function references `sound` in it's parent scope, `sound`
-		// continues to live as long as the new `speak` function does.
-		// This is closure, closure is what allows speak to reference it's external scope long
-		// after it should have been removed by the garbage collector.
-        console.log(sound);
-    }
-}
-
-//
 
 /**
  * Prototypes
@@ -247,17 +167,13 @@ Person = (function() {
  * Private Methods
  */
 Person = (function() {
-    function Person (name, age) {
+    function Person (name, yob) {
         this.name = name;
-        this.age = age;
+        this.age = calculateAge(yob);
     }
 
-    Person.prototype.speak = function() {
-        return thinkOfSomethingToSay(this);
-    };
-
-    function thinkOfSomethingToSay(person) {
-        return "Hi, I'm " + person.name;
+    function calculateAge(yob) {
+        return 2016 - yob;
     }
 
     return Person;
@@ -266,8 +182,72 @@ Person = (function() {
 bob = new Person("byyob", 25);
 bob.speak(); // Hi, I'm bob
 
+
 /**
- * Simulate private variables using closure/lexical scope.
+ * Static Methods
+ */
+var Person = (function() {
+    function Person (name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    Person.prtotype.toString = function() {
+        return this.name + " " + this.age;
+    };
+
+    Person.findById = function() {
+        $.getJSON("/api/people/" + id).then(function(response) {
+            var name = response.name;
+            var age = response.age;
+
+            return new Person(age, name);
+        });
+    };
+
+    return Person;
+})();
+
+var personId = $("#selected-user").text();
+
+var newPerson = new Person("Nolan", 23);
+var selectedPerson = Person.findById(selectedPersonId);
+
+selectedPerson.toString(); // Adam Sandler 45
+
+/**
+ * Private Constructors with the Module Pattern.
+ *  Let's use a Game of Life example
+ */
+
+var World = (function() {
+    function World() { }
+
+    function emptyWorld() {
+        return new World();
+    }
+
+    function blockWorld(topLeftLocation) {
+        var world = new World();
+
+        // setup block ...
+
+        return world;
+    }
+
+    return {
+        emptyWorld: emptyWorld,
+        blockWorld: blockWorld
+    }
+})();
+
+var emptyWorld = World.emptyWorld();
+
+var newWorld = new World(); // Uncaught TypeError: World is not a function...
+
+
+/**
+ * EXTRA: * Simulate private variables using closure/lexical scope.
  */
 Person = (function() {
     function Person(name, age) {
@@ -340,68 +320,3 @@ var Person = (function() {
 
     return Person
 })();
-
-/**
- * Static Methods
- */
-var Person = (function() {
-    function Person (name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    Person.prtotype.toString = function() {
-        return this.name + " " + this.age;
-    };
-
-    Person.findById = function() {
-        $.getJSON("/api/people/" + id).then(function(response) {
-            var name = response.name;
-            var age = response.age;
-
-            return new Person(age, name);
-        });
-    };
-
-    return Person;
-})();
-
-var personId = $("#selected-user").text();
-
-var newPerson = new Person("Nolan", 23);
-var selectedPerson = Person.findById(selectedPersonId);
-
-selectedPerson.toString(); // Adam Sandler 45
-
-/**
- * Private Constructors with the Module Pattern.
- *  Let's use a Game of Life example
- */
-
-var World = (function() {
-    function World() { }
-
-    function emptyWorld() {
-        return new World();
-    }
-
-    function blockWorld(topLeftLocation) {
-        var world = new World();
-
-        // setup block ...
-
-        return world;
-    }
-
-    return {
-        emptyWorld: emptyWorld,
-        blockWorld: blockWorld
-    }
-})();
-
-var emptyWorld = World.emptyWorld();
-
-var newWorld = new World(); // Uncaught TypeError: World is not a function...
-
-
-
